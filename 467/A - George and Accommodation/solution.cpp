553179c6700037a68738fab03255070fda4b1e0a
Abhishek Yadav <127724654+Abhi-2516@users.noreply.github.com>
2025-12-14 21:27:32 +0530

    Add George and Accommodation [A] from Codeforces

---
+++ 467/A - George and Accommodation/solution.cpp
@@ -0,0 +1,1 @@
+#include <bits/stdc++.h>using namespace std;int main() {    int n, cnt = 0;    cin >> n;    while (n--) {        int p, q;        cin >> p >> q;        if (q - p >= 2) cnt++;    }    cout << cnt;    return 0;}
