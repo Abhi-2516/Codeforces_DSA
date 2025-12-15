ea5c45f9ccfd35ed5a5cafbda21046043615e77e
Abhishek Yadav <127724654+Abhi-2516@users.noreply.github.com>
2025-12-15 16:08:33 +0530

    Add Hulk [A] from Codeforces

---
+++ 705/A - Hulk/solution.cpp
@@ -0,0 +1,1 @@
+#include <bits/stdc++.h>using namespace std;int main() {    int n;    cin >> n;    string result = "";    for (int i = 1; i <= n; i++) {        if (i % 2 == 1) result += "I hate";        else result += "I love";        if (i < n) result += " that ";    }    result += " it";    cout << result;    return 0;}
